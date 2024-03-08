2e7007101a31239f724144763583217c67e515b1
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2024-03-08 19:57:20 +0530

    structure

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+import Button from "@material-ui/core/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+    </div>
+  );
+}
+
+export default App;
