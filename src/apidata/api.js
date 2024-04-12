00bb28529ff6171fa647852aae471db0348fe6f1
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2024-04-12 19:41:12 +0530

    add

--- src/apidata/api.js
+++ src/apidata/api.js
@@ -5,6 +5,8 @@ export const CoinList = (currency) =>
 export const SingleCoin = (id) =>
   `https://api.coingecko.com/api/v3/coins/${id}`;
 
+  
+
 export const HistoricalChart = (id, days = 365, currency) =>
   `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
 
